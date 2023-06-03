11ed3b33a0a12e74c01606546100779fa6c2b32c
marcio <marciogsantana@yahoo.com.br>
2023-06-03 16:37:56 -0300

    terceiro comit

--- Backend/SomarNumeros.js
+++ Backend/SomarNumeros.js
@@ -5,5 +5,5 @@ function somarNumeros(num1, num2) {
 // chamada da função
 const resultado = somarNumeros(10, 20);
 console.log('Resultado:', resultado);
-
+console.log("testando fluxo de trabalho");
 module.exports = somarNumeros;
